5f31b2c03a8f8cbab34dbc027319d81c7c1db04d
maxim <infolebedev@gmail.com>
2026-09-11 22:02:38 +0300

    fix: Transporter company nullable

--- src/Modules/Transportation/Transporter/Domain/Entity/Transporter.php
+++ src/Modules/Transportation/Transporter/Domain/Entity/Transporter.php
@@ -22,7 +22,7 @@ class Transporter
         #[ORM\Column(name: 'transporter_status', type: 'string', enumType: TransporterStatus::class)]
         private TransporterStatus $transporterStatus,
 
-        #[ORM\Column(name: 'company_id', type: 'company_id')]
+        #[ORM\Column(name: 'company_id', type: 'company_id', nullable: true)]
         private string $companyId,
 
         #[ORM\Column(name: 'created_at', type: 'datetime')]

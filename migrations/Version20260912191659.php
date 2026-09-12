5ee8f55b22229e83dc9aa61d7254710b21713e9d
maxim <infolebedev@gmail.com>
2026-09-12 22:19:38 +0300

    fix: Migration rollup

--- migrations/Version20260912191659.php
+++ migrations/Version20260912191659.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260911190310 extends AbstractMigration
+final class Version20260912191659 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20260911190310 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE transporter ALTER company_id DROP NOT NULL');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE TABLE transporter (id UUID NOT NULL, name VARCHAR(255) NOT NULL, transporter_status VARCHAR(255) NOT NULL, company_id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_A036E2D45E237E06 ON transporter (name)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE transporter ALTER company_id SET NOT NULL');
+        $this->addSql('DROP TABLE transporter');
     }
 }
